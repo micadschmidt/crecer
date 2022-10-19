9de31836c470b5d87216e5df5ece7cb3c440dfc6
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-10-19 19:22:45 -0300

    validations profesors

--- app/models/profesor.rb
+++ app/models/profesor.rb
@@ -1,2 +1,7 @@
 class Profesor < ApplicationRecord
+  validates :descripcion_profesor, length: { minimum: 10, maximum: 400 }
+  validates :nombre, presence: true
+  validates :apellido, presence: true
+  #validates :foto_profesor, presence: true
+  #has_one_attached :foto_profesor
 end
