018b7823e3b3a51d24e1c106821815c46eb1098b
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-10-19 19:18:40 -0300

    validations users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
+  validates :nombre, presence: true
+  validates :apellido, presence: true
+  validates :alumnonombrecompleto, presence: true
   validates :email, uniqueness: { scope: :email, message: "email already has an account" }
+  has_many :pagos
+  #validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
