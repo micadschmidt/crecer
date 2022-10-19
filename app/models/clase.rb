fbd58d3f12d3b8920823f71f543ff4240923d5a6
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-10-19 19:28:19 -0300

    validations clases

--- app/models/clase.rb
+++ app/models/clase.rb
@@ -1,3 +1,10 @@
 class Clase < ApplicationRecord
   belongs_to :profesor
+
+  CATEGORIAS = ["Estimulación temprana", "Telas", "Gimnasia artística"]
+  validates :categoria, inclusion: { in: CATEGORIAS }
+  validates :descripcion, length: { minimum: 10, maximum: 400 }
+  validates :categoria, presence: true
+
+  #has_many_attached :fotos
 end
