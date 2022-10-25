7621343771d400f790c9017aecb9be4b3251e346
Micaela Dauria <micaelaschmidt47@gmail.com>
2022-10-25 16:14:29 -0300

    profesors controller done

--- app/controllers/profesors_controller.rb
+++ app/controllers/profesors_controller.rb
@@ -7,27 +7,41 @@ class ProfesorsController < ApplicationController
   end
 
   def show
-
+    @profesor = Profesor.find
   end
 
   def new
-
+    @profesor = Profesor.new
   end
 
   def create
-
+    @profesor = Profesor.new(profesor_params)
+    # @company.user_id = current_user.id
+    # authorize @company
+    if @profesor.save
+      redirect_to profesors_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
-
+    @profesor = Profesor.find
   end
 
   def update
-
+    @profesor = Profesor.find
+    if @profesor.update(profesor_params)
+      redirect_to profesor_path(@profesor)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
-
+    @profesor = Profesor.find(params[:id])
+    @profesor.destroy
+    redirect_to profesors_path, status: :see_other
   end
 
   private
