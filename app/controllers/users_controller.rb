c10bd3448b88d0262050276f7a1b550c0551a0a1
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-31 15:23:44 -0300

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  
+  before_action :set_users, only: [:show, :edit, :update]
+  # before_action :set_user
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+  def set_users
+    @user = User.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:nombre, :apellido, :alumnonombrecompleto, :foto, :email)
+  end
 end
