f662f8811d7d43b5b8cea65b1e7444c6c7837a75
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-01-31 17:00:02 -0300

    user  show view current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_users, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
   # before_action :set_user
 
   def index
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find
+    @user = User.find(params[:id])
   end
 
   def new
@@ -34,15 +34,16 @@ class UsersController < ApplicationController
   end
 
   private
-  def set_users
-    @user = User.find(params[:id])
-  end
-
   def set_user
+    @user = User.find(params[:id])
     @user = current_user
   end
 
+  # def set_user
+  #   @user = current_user
+  # end
+
   def user_params
-    params.require(:user).permit(:nombre, :apellido, :alumnonombrecompleto, :foto, :email)
+    params.require(:user).permit(:id, :nombre, :apellido, :alumnonombrecompleto, :foto, :email)
   end
 end
