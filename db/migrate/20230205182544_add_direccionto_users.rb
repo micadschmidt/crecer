496ab076158d0bc54e2d2362124ab144424d6195
Micaela Dauria <micaelaschmidt47@gmail.com>
2023-02-05 15:39:59 -0300

    adding direccion to users

---
+++ db/migrate/20230205182544_add_direccionto_users.rb
@@ -0,0 +1,5 @@
+class AddDirecciontoUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :direccion, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_05_181210) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_182544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_05_181210) do
     t.string "alumnonombrecompleto1"
     t.string "alumnonombrecompleto2"
     t.string "telefono"
+    t.string "direccion"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
